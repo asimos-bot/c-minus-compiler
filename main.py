1ae1c219fba26e082ff8e0d19a09ce21c5e194ca
Jhonatan <jhonatan_goncalves@id.uff.br>
2022-07-15 17:01:44 -0300

    seprando outputs, e escrevendo exemplos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import sys
 from cminus.lex.lex import Lex
 from cminus.sintatic.parser import Parser
 if __name__ == '__main__':
     lex: Lex = Lex()
-    tokens = lex.get_tokens_from_file('source_code.txt')
+    tokens = lex.get_tokens_from_file(f'source_code/{sys.argv[1]}.txt')
     list_of_tokens = list(tokens)
-    with open('output/token_list.txt', 'wt') as file:
+    with open(f'output/token_list.txt', 'wt') as file:
         for i, token in enumerate(list_of_tokens):
             file.write(str(i) + " " + str(token))
             file.write("\n")
