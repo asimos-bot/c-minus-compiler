877cb694f65b628fb29c5cc49e04bfc15ecb40aa
Jhonatan <jhonatan_goncalves@id.uff.br>
2022-07-15 16:29:20 -0300

    movendo arquivos de saida para /output

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ if __name__ == '__main__':
     lex: Lex = Lex()
     tokens = lex.get_tokens_from_file('source_code.txt')
     list_of_tokens = list(tokens)
-    with open('token_list.txt', 'wt') as file:
+    with open('output/token_list.txt', 'wt') as file:
         for i, token in enumerate(list_of_tokens):
             file.write(str(i) + " " + str(token))
             file.write("\n")
     parser = Parser(list_of_tokens)
     ast = parser.parse()
     print(ast.subtree_to_str())
-    ast.to_dot("arvore.dot")
+    ast.to_dot("output/arvore.dot")
